94f1ede22ae72bfdb449fb2b3d338ba5f630b10e
krish9711 <krish9711@gmail.com>
2025-01-19 18:00:56 +0530

    updated email form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,30 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Contact = () => {
 
+  const [buttonText, setButtonText] = useState("Submit");
+  const [loading, setLoading] = useState(false);
+
   const handleSubmit = (e) => {
+    e.preventDefault();
+    setButtonText("...");
+    setLoading(true);
     const scriptURL = 'https://script.google.com/macros/s/AKfycbxPa668L0WXf8EA8NBlv70SNV-bSWW9zOiEy_sVTj2TGx5-fb3r4n6CSqaFfVpi4bN39w/exec';
     const form = document.forms['submit-to-google-sheet'];
     const msg = document.getElementById("msg");
     const emailField = document.getElementById("email");
-    e.preventDefault();
+
     if (!emailField.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+      setButtonText("Submit");
+      setLoading(false);
       msg.style.color = "red";
       msg.innerHTML = "Please enter a valid email address.";
       setTimeout(function () { msg.innerHTML = ""; }, 3000);
       return;
     }
+
     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
       .then(response => {
         msg.style.color = "green";
         msg.innerHTML = "Thanks for subscribing!";
         setTimeout(function () { msg.innerHTML = ""; }, 3000);
         form.reset();
+        setButtonText("Submit");
+        setLoading(false);
       })
       .catch(error => {
         msg.style.color = "red";
         msg.innerHTML = "Oops! There was a problem.";
         setTimeout(function () { msg.innerHTML = ""; }, 3000);
+        setButtonText("Submit");
+        setLoading(false);
       });
   };
 
@@ -38,8 +51,9 @@ const Contact = () => {
             id="email"
             name="email"
             type="text"
+            disabled={loading}
             placeholder="yourmail@gmail.com" />
-          <button className="secondary-button">Go</button>
+          <button className="secondary-button" disabled={loading}>{buttonText}</button>
         </div>
         <span id="msg" className="contact-form-message"></span>
       </form>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,33 @@
 import React from "react";
 import Logo from "../assets/logo.png";
-import { BsTwitter } from "react-icons/bs";
-import { SiLinkedin } from "react-icons/si";
-import { BsYoutube } from "react-icons/bs";
-import { FaFacebookF } from "react-icons/fa";
+// import { BsTwitter } from "react-icons/bs";
+// import { SiLinkedin } from "react-icons/si";
+// import { BsYoutube } from "react-icons/bs";
+// import { FaFacebookF } from "react-icons/fa";
+import { IoMail } from "react-icons/io5";
 
 const Footer = () => {
+
+  const handleEmailClick = () => {
+    navigator.clipboard.writeText("business@knowyourtrips.com");
+    alert("Email copied to clipboard");
+  };
+
+
   return (
     <div className="footer-wrapper">
       <div className="footer-section-two">
         <div className="footer-logo-container">
-            <img src={Logo} alt="" />
+          <img src={Logo} alt="" />
         </div>
         <div className="footer-section-one">
-        <div className="footer-icons">
-          <BsTwitter />
-          <SiLinkedin />
-          <BsYoutube />
-          <FaFacebookF />
+          <div className="footer-icons">
+            <IoMail onClick={handleEmailClick} />
+          </div>
+          <div className="footer-text" onClick={handleEmailClick}>
+            business@knowyourtrips.com
+          </div>
         </div>
-        <span>knowyourtrips@gmail.com</span>
-      </div>
       </div>
     </div>
   );
